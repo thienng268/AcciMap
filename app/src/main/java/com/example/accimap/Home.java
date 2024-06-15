c78e9852f9833670435861901b586f1c458e5b01
nous366 <21521392@gm.uit.edu.vn>
2024-06-16 06:37:56 +0700

    update

--- app/src/main/java/com/example/accimap/Home.java
+++ app/src/main/java/com/example/accimap/Home.java
@@ -27,7 +27,7 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_home);
         Toast.makeText(Home.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
         FirebaseApp.initializeApp(this);
         database = FirebaseDatabase.getInstance();

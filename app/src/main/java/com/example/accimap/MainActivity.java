98338d8b2a31e744b0fd6bab831b5a9ee4a97195
nous366 <21521392@gm.uit.edu.vn>
2024-06-15 20:33:31 +0700

    tinh trang, xung quanh

--- app/src/main/java/com/example/accimap/MainActivity.java
+++ app/src/main/java/com/example/accimap/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.accimap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
